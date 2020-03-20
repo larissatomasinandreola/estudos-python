224e5cfebc7fad929a1128a401d60ceb0bfb5563
larissatomasinandreola <larissa.andreola@hotmail.com>
2020-03-20 19:34:21 -0300

    Desenvolvendo função para ler um arquivo dat

---
+++ leitor_arquivo_dat.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 19:20:53 2020
+
+@author: Larissa
+"""
+
+
+datContent = [i.strip().split() for i in open("SIMCOSTA_Tramandaí_MET_2018-03-14.tab").readlines()]
+print(datContent)
